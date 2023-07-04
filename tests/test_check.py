ab054a76b1086f7e67986f01fdea4cb9fb75b1ac
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-04 16:08:12 +0200

    test timeouts

--- tests/test_check.py
+++ tests/test_check.py
@@ -4,14 +4,26 @@ import pytest
 from website_monitor.check import check_website
 
 
+TEST_TIMEOUT_SECONDS = 8
+
+
 @pytest.mark.asyncio
 async def test_check_website():
     async with aiohttp.ClientSession() as session:
         url = "https://example.org"
 
-        result = await check_website(session, url, re.compile(regex_str_opt))
         regex_str_opt = "Example D[a-z]+"
+        result = await check_website(session, url, re.compile(regex_str_opt), timeout=TEST_TIMEOUT_SECONDS)
 
         assert result.url == url
         assert result.regex_opt == regex_str_opt
         assert result.regex_match_opt == "Example Domain"
+
+
+@pytest.mark.asyncio
+async def test_check_website_with_impossible_timeout():
+    async with aiohttp.ClientSession() as session:
+        url = "https://example.org"
+
+        with pytest.raises(TimeoutError):
+            await check_website(session, url, timeout=0.01)

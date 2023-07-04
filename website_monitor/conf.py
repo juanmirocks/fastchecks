880660644601fdf7a5989914c264e2a346373b5d
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-04 17:18:57 +0200

    simplify, just use float, which is supported

--- website_monitor/conf.py
+++ website_monitor/conf.py
@@ -1,7 +1,3 @@
 import os
-import aiohttp
-
-
 # We assume the environment variable, if any, is a valid float. Otherwise, the application will exit with an error early.
 DEFAULT_REQ_TIMEOUT_SECONDS = float(os.environ.get("DEFAULT_REQ_TIMEOUT_SECONDS", "10.0"))
-DEFAULT_REQ_TIMEOUT_CLIENT_TIMEOUT = aiohttp.ClientTimeout(total=DEFAULT_REQ_TIMEOUT_SECONDS)

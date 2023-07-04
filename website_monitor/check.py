3f4cb2f66fbc758f63f4300aadeaaabedb564dd9
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-04 15:06:49 +0200

    write code to check a website

---
+++ website_monitor/check.py
@@ -0,0 +1,49 @@
+import datetime
+import re
+import aiohttp
+from typing import NamedTuple
+from urllib.parse import urlparse
+
+
+class CheckResult(NamedTuple):
+    url: str
+    timestamp: float
+    response_time: float
+    response_status: int
+    checked_regex_opt: str | None
+    response_content_opt: str | None
+
+
+async def check_website(session: aiohttp.ClientSession, url: str, regex_ptr_opt: re.Pattern | None = None) -> CheckResult:
+    """
+    Access (GET) website's URL and return monitoring statistics.
+
+    Optionally: check if the website's content matches the input regex.
+    """
+    assert is_valid_url(url), f"Invalid URL: {url}"
+
+    timestamp_before = datetime.datetime.now().timestamp()
+
+    async with session.get(url) as response:
+        regex_str_opt, match_str_opt = None, None
+        if regex_ptr_opt is not None:
+            regex_str_opt = regex_ptr_opt.pattern
+            content = await response.text()
+            match_opt = regex_ptr_opt.search(content)
+            if match_opt is not None:
+                match_str_opt = match_opt[0]
+
+        # Get response time after (optionally) fetching the website's content (i.e., if the input regex is not None)
+        timestamp_after = datetime.datetime.now().timestamp()
+        response_time = timestamp_after - timestamp_before
+
+        return CheckResult(url, timestamp_before, response_time, response.status, regex_str_opt, match_str_opt)
+
+
+# See: https://snyk.io/blog/secure-python-url-validation/
+def is_valid_url(url: str) -> bool:
+    try:
+        result = urlparse(url)
+        return (result.scheme != "") and (result.netloc != "")
+    except:
+        return False
---
+++ website_monitor/check_quick_website.py
@@ -0,0 +1,24 @@
+import asyncio
+import re
+import aiohttp
+import sys
+
+from website_monitor.check import check_website
+
+
+async def main(url: str, regex_str_opt: str | None = None):
+    regex_ptr_opt = None if regex_str_opt is None else re.compile(regex_str_opt)
+
+    async with aiohttp.ClientSession() as session:
+        result = await check_website(session, url, regex_ptr_opt)
+        print(result)
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) in (2, 3), "Usage: python website_monitor.check_quick_website <url> [regex]"
+    # Assert number of parameters is 1, 2, or 3
+
+    url = sys.argv[1]
+    regex_str_opt = sys.argv[2] if len(sys.argv) == 3 else None
+
+    asyncio.run(main(url, regex_str_opt))

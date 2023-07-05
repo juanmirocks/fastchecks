412be856dd0ed29f5cc31a5acd8fb3772efba364
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-05 17:03:08 +0200

    update script to support writing

--- website_monitor/check_quick.py
+++ website_monitor/check_quick.py
@@ -5,13 +5,19 @@ import sys
 from website_monitor.check import check_website
 from website_monitor.sockets.postgres import CheckResultSocketPostgres
 from website_monitor import conf
-from website_monitor.types import WebsiteCheck
+from website_monitor.types import CheckResult, WebsiteCheck
 
 
-async def check_single_website_only(check: WebsiteCheck) -> None:
+async def check_website_only(check: WebsiteCheck) -> CheckResult:
     async with aiohttp.ClientSession() as session:
-        result = await check_website(session, check)
-        print(result)
+        ret = await check_website(session, check)
+        print(ret)
+        return ret
+
+
+async def write_result(result: CheckResult) -> None:
+    async with await CheckResultSocketPostgres.create(conf.get_postgres_conninfo()) as socket:
+        await socket.write(result)
 
 
 async def read_all_results() -> None:
@@ -20,7 +26,7 @@ async def read_all_results() -> None:
             print(result)
 
 
-if __name__ == "__main__":
+async def main() -> None:
     assert len(sys.argv) in (3, 4), "Usage: python -m website_monitor.check_quick <opr> <url> [regex]"
 
     opr = sys.argv[1]
@@ -28,8 +34,19 @@ if __name__ == "__main__":
     regex_str_opt = sys.argv[3] if len(sys.argv) == 4 else None
 
     match opr:
-        case "check_single_website_only":
-            asyncio.run(check_single_website_only(WebsiteCheck.create_with_validation(url, regex_str_opt)))
+        case "check_website_only":
+            await check_website_only(WebsiteCheck.create_with_validation(url, regex_str_opt))
+
+        case "check_website_and_write":
+            result = await check_website_only(WebsiteCheck.create_with_validation(url, regex_str_opt))
+            await write_result(result)
 
         case "read_all_results":
-            asyncio.run(read_all_results())
+            await read_all_results()
+
+        case _:
+            raise ValueError(f"Unknown opr: {opr}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

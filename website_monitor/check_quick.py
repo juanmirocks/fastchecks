071ede5f5552a048351623c158eacfc3a2aa40a2
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-05 17:17:44 +0200

    simplify execution with a common context

--- website_monitor/check_quick.py
+++ website_monitor/check_quick.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import NamedTuple
 import aiohttp
 import sys
 
@@ -7,24 +8,35 @@ from website_monitor.sockets.postgres import CheckResultSocketPostgres
 from website_monitor import conf
 from website_monitor.types import CheckResult, WebsiteCheck
 
+# -----------------------------------------------------------------------------
 
-async def check_website_only(check: WebsiteCheck) -> CheckResult:
-    async with aiohttp.ClientSession() as session:
-        ret = await check_website(session, check)
-        print(ret)
-        return ret
+class Context(NamedTuple):
+    session: aiohttp.ClientSession
+    socket: CheckResultSocketPostgres
+
+    async def __aenter__(self) -> "Context":
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
+        await asyncio.gather(self.session.close(), self.socket.close())
+
+# -----------------------------------------------------------------------------
+
+async def check_website_only(ctx: Context, check: WebsiteCheck) -> CheckResult:
+    ret = await check_website(ctx.session, check)
+    print(ret)
+    return ret
 
 
-async def write_result(result: CheckResult) -> None:
-    async with await CheckResultSocketPostgres.create(conf.get_postgres_conninfo()) as socket:
-        await socket.write(result)
+async def write_result(ctx: Context, result: CheckResult) -> None:
+    await ctx.socket.write(result)
 
 
-async def read_all_results() -> None:
-    async with await CheckResultSocketPostgres.create(conf.get_postgres_conninfo()) as socket:
-        async for result in socket.read_all():
-            print(result)
+async def read_all_results(ctx: Context) -> None:
+    async for result in ctx.socket.read_all():
+        print(result)
 
+# -----------------------------------------------------------------------------
 
 async def main() -> None:
     assert len(sys.argv) in (3, 4), "Usage: python -m website_monitor.check_quick <opr> <url> [regex]"
@@ -33,19 +45,22 @@ async def main() -> None:
     url = sys.argv[2]
     regex_str_opt = sys.argv[3] if len(sys.argv) == 4 else None
 
-    match opr:
-        case "check_website_only":
-            await check_website_only(WebsiteCheck.create_with_validation(url, regex_str_opt))
+    ctx = Context(session=aiohttp.ClientSession(), socket=await CheckResultSocketPostgres.create(conf.get_postgres_conninfo()))
 
-        case "check_website_and_write":
-            result = await check_website_only(WebsiteCheck.create_with_validation(url, regex_str_opt))
-            await write_result(result)
+    async with ctx:
+        match opr:
+            case "check_website_only":
+                await check_website_only(ctx, WebsiteCheck.create_with_validation(url, regex_str_opt))
 
-        case "read_all_results":
-            await read_all_results()
+            case "check_website_and_write":
+                result = await check_website_only(ctx, WebsiteCheck.create_with_validation(url, regex_str_opt))
+                await write_result(ctx, result)
 
-        case _:
-            raise ValueError(f"Unknown opr: {opr}")
+            case "read_all_results":
+                await read_all_results(ctx)
+
+            case _:
+                raise ValueError(f"Unknown opr: {opr}")
 
 
 if __name__ == "__main__":

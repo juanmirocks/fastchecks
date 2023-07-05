44a1bdf3e54beeed53a874adca392b073611b271
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-05 16:18:36 +0200

    define interface WebsiteCheckSocket

--- website_monitor/sockets/__init__.py
+++ website_monitor/sockets/__init__.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Iterator
+from typing import AsyncIterator
 
 from website_monitor.types import CheckResult
 
@@ -10,7 +10,7 @@ class CheckResultSocket(ABC):
         ...
 
     @abstractmethod
-    async def read_all(self) -> Iterator[CheckResult]:
+    async def read_all(self) -> AsyncIterator[CheckResult]:
         ...
 
     @abstractmethod
@@ -18,6 +18,26 @@ class CheckResultSocket(ABC):
         ...
 
     async def __aenter__(self) -> "CheckResultSocket":
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        await self.close()
+
+
+class WebsiteCheckSocket(ABC):
+    @abstractmethod
+    async def write(self, check: CheckResult) -> None:
+        ...
+
+    @abstractmethod
+    async def read_all(self) -> AsyncIterator[AsyncIterator]:
+        ...
+
+    @abstractmethod
+    async def close(self) -> None:
+        ...
+
+    async def __aenter__(self) -> "WebsiteCheckSocket":
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback) -> None:

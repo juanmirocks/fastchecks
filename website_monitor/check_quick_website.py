6a68f8c726760a2315d04914be6dd641a1f29cad
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-05 14:47:43 +0200

    add operation read_all_results

--- website_monitor/check_quick_website.py
+++ website_monitor/check_quick_website.py
@@ -4,9 +4,11 @@ import aiohttp
 import sys
 
 from website_monitor.check import check_website
+from website_monitor.sockets.postgres import CheckResultSocketPostgres
+from website_monitor import conf
 
 
-async def main(url: str, regex_str_opt: str | None = None):
+async def check_single_website_only(url: str, regex_str_opt: str | None = None) -> None:
     regex_ptr_opt = None if regex_str_opt is None else re.compile(regex_str_opt)
 
     async with aiohttp.ClientSession() as session:
@@ -14,11 +16,22 @@ async def main(url: str, regex_str_opt: str | None = None):
         print(result)
 
 
+async def read_all_results() -> None:
+    async with await CheckResultSocketPostgres.create(conf.get_postgres_conninfo()) as socket:
+        async for row in socket.read_all():
+            print(row)
+
+
 if __name__ == "__main__":
-    assert len(sys.argv) in (2, 3), "Usage: python -m website_monitor.check_quick_website <url> [regex]"
-    # Assert number of parameters is 2 or 3
+    assert len(sys.argv) in (3, 4), "Usage: python -m website_monitor.check_quick_website <opr> <url> [regex]"
 
-    url = sys.argv[1]
-    regex_str_opt = sys.argv[2] if len(sys.argv) == 3 else None
+    opr = sys.argv[1]
+    url = sys.argv[2]
+    regex_str_opt = sys.argv[3] if len(sys.argv) == 4 else None
 
-    asyncio.run(main(url, regex_str_opt))
+    match opr:
+        case "check_single_website_only":
+            asyncio.run(check_single_website_only(url, regex_str_opt))
+
+        case "read_all_results":
+            asyncio.run(read_all_results())

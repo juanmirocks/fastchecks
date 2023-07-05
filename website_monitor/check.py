6f1d24c6aff5da45a17493a8b367f529b2e46c02
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-05 15:46:31 +0200

    move utils to own file

--- website_monitor/check.py
+++ website_monitor/check.py
@@ -5,6 +5,7 @@ from urllib.parse import urlparse
 
 from website_monitor import conf
 from website_monitor.types import CheckResult
+from website_monitor.util import get_utcnow, get_utcnow_time_difference_seconds, validate_url
 
 
 async def check_website(session: aiohttp.ClientSession, url: str, regex_ptr_opt: re.Pattern | None = None, timeout: float = None) -> CheckResult:
@@ -58,60 +59,3 @@ async def search_pattern_whole_text_body(regex_ptr: re.Pattern, response: aiohtt
         return match_opt[0]
     else:
         return None
-
-
-def validate_url(url: str, raise_error: bool = True) -> str | None:
-    """
-    Validate given string is a valid URL.
-
-    If the URL is valid, return its netloc (e.g. "www.example.com").
-    Else if raise_error is True, raise ValueError.
-    """
-    # See: https://snyk.io/blog/secure-python-url-validation/
-
-    ret: str
-    try:
-        result = urlparse(url)
-        ret = result.scheme and result.netloc
-    except:
-        ret = None
-
-    if ret:
-        return ret
-    elif raise_error:
-        raise ValueError(f"Invalid URL: {url}")
-    else:
-        # ret could have been (without exception) the empty string (which is also falsy), but we return None to avoid confusions
-        return None
-
-
-def validate_regex(regex_str: str, raise_error: bool = True) -> re.Pattern | None:
-    """
-    Validate regex string: the regex must be compilable.
-
-    If the regex is valid, return its re.Pattern.
-    Else if raise_error is True, raise ValueError.
-    """
-    try:
-        return re.compile(regex_str)
-    except re.error:
-        if raise_error:
-            raise ValueError(f"Invalid regex (cannot compile it): {regex_str}")
-        else:
-            return None
-
-
-def get_utcnow() -> datetime.datetime:
-    """
-    Return the current UTC timestamp in seconds.
-
-    Use this method to make sure you are always using a timestamp with same timezone (UTC).
-    """
-    return datetime.datetime.utcnow()
-
-
-def get_utcnow_time_difference_seconds(timestamp_start: datetime.datetime) -> float:
-    """
-    Return the time difference in seconds between the current UTC datetime and the input datetime.
-    """
-    return (get_utcnow() - timestamp_start).total_seconds()

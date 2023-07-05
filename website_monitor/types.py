3ae592acf102f50b43da3efd98a40ba3ffadab4d
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-05 11:55:35 +0200

    replace NamedTuple by Pydantic's BaseModel for type safety

--- website_monitor/types.py
+++ website_monitor/types.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import Iterator, NamedTuple
+from typing import Iterator
+from pydantic import BaseModel
 
 
-class CheckResult(NamedTuple):
+# We use Pydantic for type safety
+# See benchmarks with other alternatives (e.g. NamedTuple): https://janhendrikewers.uk/pydantic_vs_protobuf_vs_namedtuple_vs_dataclasses.html
+# Note that Pydantic V2 (released in 2023 June) promises a 5-50x speedup over V1: https://docs.pydantic.dev/2.0/blog/pydantic-v2-alpha/#headlines
+class CheckResult(BaseModel):
     url: str
     timestamp_start: float
     response_time: float

d8dadaa13ce1809d3a4f5d307247b5c39554ead3
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-06 17:18:27 +0200

    doc

--- website_monitor/types.py
+++ website_monitor/types.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from website_monitor.util import validate_regex, validate_url
 
 
-# We use Pydantic classes for type safety
+# We use Pydantic classes for type safety and because they could be handy in the future for de/serialization.
 # See benchmarks with other alternatives (e.g. NamedTuple): https://janhendrikewers.uk/pydantic_vs_protobuf_vs_namedtuple_vs_dataclasses.html
 # Note that Pydantic V2 (released in 2023 June) promises a 5-50x speedup over V1: https://docs.pydantic.dev/2.0/blog/pydantic-v2-alpha/#headlines
 

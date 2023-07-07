e4d2287e5ee763937d226370b3f70129e8b40ce6
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-07 19:27:44 +0200

    include down sql script

---
+++ fastchecks/sockets/postgres/schema/down.sql
@@ -0,0 +1,5 @@
+-- Use for testing and experimenting only
+-- Drop all objects in the public schema
+-- Ref: https://stackoverflow.com/questions/3327312/how-can-i-drop-all-the-tables-in-a-postgresql-database
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;

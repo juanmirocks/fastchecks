1e9043fe72d23fb03593a4000c0da1d71b0c700a
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-08 20:45:57 +0200

    rename fields

--- fastchecks/checks_runner.py
+++ fastchecks/checks_runner.py
@@ -18,21 +18,21 @@ class ChecksRunnerContext:
     """
 
     def __init__(
-        self, session: aiohttp.ClientSession, checks_socket: WebsiteCheckSocket, results_socket: CheckResultSocket
+        self, session: aiohttp.ClientSession, checks: WebsiteCheckSocket, results: CheckResultSocket
     ) -> None:
         require(not session.closed, "Session must be open")
-        require(not checks_socket.is_closed(), "Checks socket must be open")
-        require(not results_socket.is_closed(), "Results socket must be open")
+        require(not checks.is_closed(), "Checks socket must be open")
+        require(not results.is_closed(), "Results socket must be open")
 
-        self._session = session
-        self.checks_socket = checks_socket
-        self.results_socket = results_socket
+        self._aiohttp_session = session
+        self.checks = checks
+        self.results = results
 
     async def __aenter__(self) -> "ChecksRunnerContext":
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
-        await asyncio.gather(self._session.close(), self.checks_socket.close(), self.results_socket.close())
+        await asyncio.gather(self._aiohttp_session.close(), self.checks.close(), self.results.close())
 
     async def close(self) -> None:
         await self.__aexit__(None, None, None)
@@ -43,37 +43,37 @@ class ChecksRunnerContext:
     def init_with_postgres(cls, postgres_conninfo: str) -> "ChecksRunnerContext":
         return cls(
             session=aiohttp.ClientSession(),
-            checks_socket=WebsiteCheckSocketPostgres(postgres_conninfo),
-            results_socket=CheckResultSocketPostgres(postgres_conninfo),
+            checks=WebsiteCheckSocketPostgres(postgres_conninfo),
+            results=CheckResultSocketPostgres(postgres_conninfo),
         )
 
     # -----------------------------------------------------------------------------
 
     async def upsert_check(self, check: WebsiteCheck) -> None:
-        await self.checks_socket.upsert(check)
+        await self.checks.upsert(check)
 
     async def read_all_checks(self) -> None:
-        async for check in self.checks_socket.read_last_n(util.PRACTICAL_MAX_INT):
+        async for check in self.checks.read_last_n(util.PRACTICAL_MAX_INT):
             print(check)
 
     async def delete_check(self, url: str) -> None:
-        await self.checks_socket.delete(url)
+        await self.checks.delete(url)
 
     # -----------------------------------------------------------------------------
 
     async def check_website_only(self, check: WebsiteCheck) -> CheckResult:
-        ret = await check_website(self._session, check)
+        ret = await check_website(self._aiohttp_session, check)
         print(ret)
         return ret
 
     async def write_result(self, result: CheckResult) -> None:
-        await self.results_socket.write(result)
+        await self.results.write(result)
 
     async def check_all_websites_and_write(self) -> None:
-        async for check in self.checks_socket.read_last_n(util.PRACTICAL_MAX_INT):
+        async for check in self.checks.read_last_n(util.PRACTICAL_MAX_INT):
             result = await self.check_website_only(check)
             await self.write_result(result)
 
     async def read_last_n_results(self, n: int) -> None:
-        async for result in self.results_socket.read_last_n(n):
+        async for result in self.results.read_last_n(n):
             print(result)

0c3797f17d6d5eccd87ea850960141800d941e3f
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-09 14:29:50 +0200

    ignore program-exit-like exceptions in the cli

--- fastchecks/cli.py
+++ fastchecks/cli.py
@@ -51,4 +51,8 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        # ignore program-exit-like exceptions in the cli
+        pass

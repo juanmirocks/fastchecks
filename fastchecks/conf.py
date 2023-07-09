2747a4df4e97172a92b7023030b3b783b6a73719
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-09 14:23:53 +0200

    define default conf values for intervals: min, max, default

--- fastchecks/conf.py
+++ fastchecks/conf.py
@@ -1,5 +1,6 @@
 import os
 from typing import Callable, TypeVar
+from fastchecks import require
 
 
 _CONVERSION_OUTPUT = TypeVar("_CONVERSION_OUTPUT")
@@ -31,12 +32,23 @@ def read_envar_value(envar_name: str, envar_value: _CONVERSION_OUTPUT | None) ->
 # -----------------------------------------------------------------------------
 
 
-DEFAULT_REQ_TIMEOUT_SECONDS = get_typed_envar(
+DEFAULT_REQ_TIMEOUT_SECONDS: float = get_typed_envar(
     "FC_DEFAULT_REQ_TIMEOUT_SECONDS", default=10.0, conversion=lambda x: float(x)
 )
 
+MIN_INTERVAL_SECONDS: int = get_typed_envar("FC_MIN_INTERVAL_SECONDS", default=5, conversion=lambda x: int(x))
 
-_POSTGRES_CONNINFO = os.environ.get("FC_POSTGRES_CONNINFO")
+MAX_INTERVAL_SECONDS: int = get_typed_envar("FC_MAX_INTERVAL_SECONDS", default=300, conversion=lambda x: int(x))
+
+DEFAULT_INTERVAL_SECONDS: int = get_typed_envar("FC_DEFAULT_INTERVAL_SECONDS", default=180, conversion=lambda x: int(x))
+require(
+    MIN_INTERVAL_SECONDS <= DEFAULT_INTERVAL_SECONDS <= MAX_INTERVAL_SECONDS,
+    f"Interval {DEFAULT_INTERVAL_SECONDS} must be between min ({MIN_INTERVAL_SECONDS}) and max ({MAX_INTERVAL_SECONDS})",
+)
+
+# -----------------------------------------------------------------------------
+
+_POSTGRES_CONNINFO: str | None = os.environ.get("FC_POSTGRES_CONNINFO")
 
 
 def get_postgres_conninfo() -> str:

acea15fc717ae163ad6f5f7ba8e611a3f7863dce
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-10 17:44:51 +0200

    test & doc

--- tests/test_vutil.py
+++ tests/test_vutil.py
@@ -9,8 +9,14 @@ def test_validate_regex():
     with pytest.raises(ValueError):
         validate_regex("*", raise_error=True)
 
-    # Possible vulnerable regex
+    #
+    # Possible vulnerable regex'ex -- accepted yet google-re2 will handle them properly without ReDOS / catastrophic backtracking
+    #
+
     # See:
     # https://learn.snyk.io/lessons/redos/javascript/
     # https://snyk.io/blog/redos-and-catastrophic-backtracking/
     assert validate_regex("A(B|C+)+D") is not None
+
+    # regex that matches any string and captures it
+    assert validate_regex("(.|\r|\n)*") is not None

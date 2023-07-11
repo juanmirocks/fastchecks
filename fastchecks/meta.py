b0c33542f089a277932fd590feb0e477035ec57c
Dr. Juan Miguel Cejuela <i@juanmi.rocks>
2023-07-11 12:03:30 +0200

    provide back up

--- fastchecks/meta.py
+++ fastchecks/meta.py
@@ -1,6 +1,10 @@
+from importlib import metadata
 import tomllib
 
+_MODULE_NAME = "fastchecks"
+
 try:
+    # TODO: the file is not available when installed as a package
     with open("pyproject.toml", "rb") as f:
         _META = tomllib.load(f)
 
@@ -14,7 +18,12 @@ except FileNotFoundError:
     # See: https://github.com/python-poetry/poetry/issues/273
     _META = {}
 
-    NAME = "fastchecks"
-    VERSION = ""
+    NAME = _MODULE_NAME
+
+    try:
+        VERSION = metadata.version(_MODULE_NAME)
+    except:
+        VERSION = ""
+
     DESCRIPTION = "🚥 Fast website monitoring backend service"
     WEBSITE = "https://github.com/juanmirocks/fastchecks"
